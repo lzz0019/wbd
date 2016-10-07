e748ee67076b8025a43993894df3e27fb215638b
lzz0019 <lzz0019@172.17.10.165>
2016-10-07 16:49:22 -0500

    Fix defects from CAO1

--- Navigation/prod/Angle.py
+++ Navigation/prod/Angle.py
@@ -6,6 +6,7 @@ Created on Sep 4, 2016
 
 import math
 from __builtin__ import str
+from scipy.constants.constants import degree
 
 class Angle():
     def __init__(self):
@@ -13,29 +14,30 @@ class Angle():
 
     def setDegrees(self, degrees=None):
         if degrees is None:
-            degrees = 0.0      
-        if(not(isinstance(degrees, int and float))):
-            raise ValueError("Angle      setDegrees:  The input should be an integer or a float!")
-        self.angle = degrees % 360.0
-        return round(self.angle, 1)  
+            degrees = 0.0
+        elif(not isinstance(degrees, int)  and not isinstance(degrees, float)):
+            raise ValueError("Angle.setDegrees: The input should be an integer or a float!")
+        else:
+            self.angle = degrees % 360.0
+        return self.angle 
     
     def setDegreesAndMinutes(self, angleString):
         separator = "d"
         index = angleString.find(separator)
         if index == -1:
-            raise ValueError("Angle      setDegreesAndMinutes:  Missing separator!")
+            raise ValueError("Angle.setDegreesAndMinutes: Missing separator!")
         X = angleString[:index]  # degree as substring
         Y = angleString[(index + 1):]  # minutes as substring
         # now need to determine if X is legal? if illegal, raise a ValueError
         if self.isLegal_X(X) == False:
-            raise ValueError("Angle      setDegreesAndMinutes:  Degree portion of angleString is illegal!")
+            raise ValueError("Angle.setDegreesAndMinutes: Degree portion of angleString is illegal!")
         # now need to determine if Y is legal? if illegal, raise a ValueError
         if self.isLegal_Y(Y) == False:
-            raise ValueError("Angle      setDegreesAndMinutes:  Minute portion of angleString is illegal!")
+            raise ValueError("Angle.setDegreesAndMinutes: Minute portion of angleString is illegal!")
         # if both legal, set the degree&minutes, return value as format"45.0" 
         degree = int(X)
         minutes = float(Y)
-        self.angle = round((degree % 360 + minutes / 60), 1)
+        self.angle = degree % 360 + minutes / 60
         return self.angle
     
     def isLegal_X(self, X):
@@ -57,21 +59,27 @@ class Angle():
         else:
             return True
         
-    def add(self, angle):
+    def add(self, angle=None):
+        if angle is None:
+            raise ValueError("Angle.add: missing parameter!")
         if not(isinstance(angle, Angle)):
-            raise ValueError("Angle      add:  angle is not an instance of Angle!")
+            raise ValueError("Angle.add: angle is not an instance of Angle!")
         self.angle = (self.angle + angle.angle) % 360.0
         return self.angle
     
-    def subtract(self, angle):
+    def subtract(self, angle=None):
+        if angle is None:
+            raise ValueError("Angle.subtract: missing parameter!")
         if not(isinstance(angle, Angle)):
-            raise ValueError("Angle      add:  angle is not an instance of Angle!")
+            raise ValueError("Angle.subtract: angle is not an instance of Angle!")
         self.angle = (self.angle - angle.angle) % 360.0
         return self.angle
     
-    def compare(self, angle):
+    def compare(self, angle=None):
+        if angle is None:
+            raise ValueError("Angle.compare: missing parameter!")
         if not(isinstance(angle, Angle)):
-            raise ValueError("Angle      compare:  angle is not an instance of Angle!")
+            raise ValueError("Angle.compare: angle is not an instance of Angle!")
         if self.angle < angle.angle:
             return -1
         elif self.angle == angle.angle:
@@ -87,6 +95,5 @@ class Angle():
         return string
     
     def getDegrees(self):
-        degree = round(self.angle, 1)
-        return degree
+        return self.angle
     
